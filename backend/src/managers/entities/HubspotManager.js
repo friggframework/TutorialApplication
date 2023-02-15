c86db8223e1cbda47ef068e42485c07de56aab2e
Jonathan O'Donnell <joncodo@gmail.com>
2023-02-15 10:24:09 -0400

    add a new integration and have it show up in the UI

---
+++ backend/src/managers/entities/HubspotManager.js
@@ -0,0 +1,9 @@
+const { ModuleManager } = require('@friggframework/api-module-salesforce');
+
+class HubspotManager extends ModuleManager {
+    constructor(params) {
+        return super(params);
+    }
+}
+
+module.exports = HubspotManager;
